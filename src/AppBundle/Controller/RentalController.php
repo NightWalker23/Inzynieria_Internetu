499eea0cb03e58b45d60756d066aff55c15b8c84
NightWalker23 <taptnw@gmail.com>
2017-10-23 15:29:18 +0200

    po drugim wykladzie

---
+++ src/AppBundle/Controller/RentalController.php
@@ -0,0 +1,136 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use AppBundle\Entity\Rental;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * Rental controller.
+ *
+ * @Route("rental")
+ */
+class RentalController extends Controller
+{
+    /**
+     * Lists all rental entities.
+     *
+     * @Route("/", name="rental_index")
+     * @Method("GET")
+     */
+    public function indexAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $rentals = $em->getRepository('AppBundle:Rental')->findAll();
+
+        return $this->render('rental/index.html.twig', array(
+            'rentals' => $rentals,
+        ));
+    }
+
+    /**
+     * Creates a new rental entity.
+     *
+     * @Route("/new", name="rental_new")
+     * @Method({"GET", "POST"})
+     */
+    public function newAction(Request $request)
+    {
+        $rental = new Rental();
+        $form = $this->createForm('AppBundle\Form\RentalType', $rental);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($rental);
+            $em->flush();
+
+            return $this->redirectToRoute('rental_show', array('id' => $rental->getId()));
+        }
+
+        return $this->render('rental/new.html.twig', array(
+            'rental' => $rental,
+            'form' => $form->createView(),
+        ));
+    }
+
+    /**
+     * Finds and displays a rental entity.
+     *
+     * @Route("/{id}", name="rental_show")
+     * @Method("GET")
+     */
+    public function showAction(Rental $rental)
+    {
+        $deleteForm = $this->createDeleteForm($rental);
+
+        return $this->render('rental/show.html.twig', array(
+            'rental' => $rental,
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+     * Displays a form to edit an existing rental entity.
+     *
+     * @Route("/{id}/edit", name="rental_edit")
+     * @Method({"GET", "POST"})
+     */
+    public function editAction(Request $request, Rental $rental)
+    {
+        $deleteForm = $this->createDeleteForm($rental);
+        $editForm = $this->createForm('AppBundle\Form\RentalType', $rental);
+        $editForm->handleRequest($request);
+
+        if ($editForm->isSubmitted() && $editForm->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('rental_edit', array('id' => $rental->getId()));
+        }
+
+        return $this->render('rental/edit.html.twig', array(
+            'rental' => $rental,
+            'edit_form' => $editForm->createView(),
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+     * Deletes a rental entity.
+     *
+     * @Route("/{id}", name="rental_delete")
+     * @Method("DELETE")
+     */
+    public function deleteAction(Request $request, Rental $rental)
+    {
+        $form = $this->createDeleteForm($rental);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->remove($rental);
+            $em->flush();
+        }
+
+        return $this->redirectToRoute('rental_index');
+    }
+
+    /**
+     * Creates a form to delete a rental entity.
+     *
+     * @param Rental $rental The rental entity
+     *
+     * @return \Symfony\Component\Form\Form The form
+     */
+    private function createDeleteForm(Rental $rental)
+    {
+        return $this->createFormBuilder()
+            ->setAction($this->generateUrl('rental_delete', array('id' => $rental->getId())))
+            ->setMethod('DELETE')
+            ->getForm()
+        ;
+    }
+}
---
+++ src/AppBundle/Entity/Book.php
@@ -0,0 +1,191 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Book
+ *
+ * @ORM\Table(name="book")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\BookRepository")
+ */
+class Book
+{
+    /**
+     * @ORM\ManyToOne(targetEntity="Author")
+     * @ORM\JoinColumn(name="author_id", referencedColumnName="id")
+     */
+    private $author;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="Category")
+     * @ORM\JoinColumn(name="category_id", referencedColumnName="id")
+     */
+    private $category;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="Publisher")
+     * @ORM\JoinColumn(name="publisher_id", referencedColumnName="id")
+     */
+    private $publisher;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="title", type="string", length=255)
+     */
+    private $title;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="amount", type="integer")
+     */
+    private $amount;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set title
+     *
+     * @param string $title
+     *
+     * @return Book
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    /**
+     * Get title
+     *
+     * @return string
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * Set amount
+     *
+     * @param integer $amount
+     *
+     * @return Book
+     */
+    public function setAmount($amount)
+    {
+        $this->amount = $amount;
+
+        return $this;
+    }
+
+    /**
+     * Get amount
+     *
+     * @return int
+     */
+    public function getAmount()
+    {
+        return $this->amount;
+    }
+
+    /**
+     * Set author
+     *
+     * @param \AppBundle\Entity\Author $author
+     *
+     * @return Book
+     */
+    public function setAuthor(\AppBundle\Entity\Author $author = null)
+    {
+        $this->author = $author;
+
+        return $this;
+    }
+
+    /**
+     * Get author
+     *
+     * @return \AppBundle\Entity\Author
+     */
+    public function getAuthor()
+    {
+        return $this->author;
+    }
+
+    /**
+     * Set category
+     *
+     * @param \AppBundle\Entity\Category $category
+     *
+     * @return Book
+     */
+    public function setCategory(\AppBundle\Entity\Category $category = null)
+    {
+        $this->category = $category;
+
+        return $this;
+    }
+
+    /**
+     * Get category
+     *
+     * @return \AppBundle\Entity\Category
+     */
+    public function getCategory()
+    {
+        return $this->category;
+    }
+
+    /**
+     * Set publisher
+     *
+     * @param \AppBundle\Entity\Publisher $publisher
+     *
+     * @return Book
+     */
+    public function setPublisher(\AppBundle\Entity\Publisher $publisher = null)
+    {
+        $this->publisher = $publisher;
+
+        return $this;
+    }
+
+    /**
+     * Get publisher
+     *
+     * @return \AppBundle\Entity\Publisher
+     */
+    public function getPublisher()
+    {
+        return $this->publisher;
+    }
+
+    public function __toString()
+    {
+        return $this->title;
+    }
+}
